411e908ace0d4d0038a6a229aa0cac7c53cae3d2
Atul Sharma <theatsharma@gmail.com>
2026-01-12 14:24:44 +0530

    Update image priotity

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -85,6 +85,7 @@ export default function HeroSection({ loading, user }) {
             height={400}
             className="w-full h-96 object-cover rounded-lg"
             priority
+            fetchPriority="high"
           />
         </div>
       </div>
